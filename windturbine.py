53221d63df2848e85b378a850a66500cb74c5e9e
felipejbs <felipejeronimobs@gmail.com>
2025-01-16 00:09:05 -0300

    feat: adicionando get data

--- windturbine.py
+++ windturbine.py
@@ -31,3 +31,20 @@ file_sensor_task = FileSensor(
     poke_interval = 10,
     dag=dag
     )
+
+def process_file(**kwarg):
+    with open(Variable.get('path_file')) as f:
+        data = json.load(f)
+        kwarg['ti'].xcom_psuh(key='idtemp', value=data['idtemp'])
+        kwarg['ti'].xcom_psuh(key='powerfactor', value=data['powerfactor'])
+        kwarg['ti'].xcom_psuh(key='hydraulicpressure', value=data['hydraulicpressure'])
+        kwarg['ti'].xcom_psuh(key='temperature', value=data['temperature'])
+        kwarg['ti'].xcom_psuh(key='timestamp', value=data['timestamp'])
+    os.remove(Variable.get('path_file'))
+
+get_data = PythonOperator(
+    task_id = 'get_data',
+    python_callable = process_file,
+    provide_context = True,
+    dag=dag
+)
